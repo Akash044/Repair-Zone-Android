6531cc9fe711b8d7209a794692e4da77373eb03f
Akash374 <akash.hossain@dataphilics.in>
2022-06-29 23:40:50 +0600

    add functionality at cart

--- components/screens/Cart.js
+++ components/screens/Cart.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { DataTable, TouchableRipple, Button } from 'react-native-paper';
 import CustomBottomNavBar from './CustomBottomNavBar'
@@ -12,25 +12,46 @@ const Cart = (props) => {
   const [distinctItems, setDistinctItems] = useState([])
   const [counts, setCounts] = useState({})
   const navigation = useNavigation()
-  console.log("all cart items--> ", cartItems)
-  // let counts = {}
-  useEffect(() => {
-    // setCounts({})
-    const distinctItem = cartItems.items.filter((item, index, self) => self.indexOf(item) === index)
-    setDistinctItems(distinctItem)
+  console.log("all cart items--> ", cartItems.items)
 
+  useEffect(() => {
+
+    let uniqueObjArray = [
+      ...new Map(cartItems.items.map((item) => [item["name"], item])).values(),
+    ];
+    // console.log(uniqueObjArray)
+    setDistinctItems(uniqueObjArray)
     cartItems.items.forEach((i) => counts[i.name] = (counts[i.name] || 0) + 1);
 
-    // distinctItems.map((item, index, self) => {
-    //   const newItem = {...item,count:counts[item.name]}
-    //   // console.log("from update--> ",)
-    //   setShowItems([...showItems,newItem])
-    //   console.log("showItems list-->",showItems)
-
-    // })
-    // console.table(distinctItem)
   }, [cartItems.items.length])
-  console.log("after update-->", distinctItems, counts)
+  // console.log("after update-->", distinctItems, counts)
+  const handleRemoveItems = (name) => {
+
+    const afterRemove = distinctItems.filter(item => item.name != name)
+    // console.log("after remove-->", afterRemove)
+    setDistinctItems(afterRemove)
+    setCounts({})
+    setCartItems({ ...cartItems, items: afterRemove })
+
+    // cartItems.items.forEach((i) => counts[i.name] = (counts[i.name] || 0) + 1);
+
+  }
+  const handlePlusBtn = (id) => {
+    console.log(id)
+    const newItem = distinctItems.filter(item => item.id === id);
+    setCounts({})
+    setCartItems({ ...cartItems, items: [newItem[0], ...cartItems.items] })
+    cartItems.items.forEach((i) => counts[i.name] = (counts[i.name] || 0) + 1);
+  }
+
+  const handleMinusBtn = (id) => {
+    console.log(id)
+    const restItems = distinctItems.filter(item => item.id !== id);
+    setCounts({})
+    setCartItems({ ...cartItems, items: restItems })
+    cartItems.items.forEach((i) => counts[i.name] = (counts[i.name] || 0) + 1);
+  }
+
   return (
     <>
       <View style={{ flex: 3 }}>
@@ -42,47 +63,49 @@ const Cart = (props) => {
             <DataTable.Title numeric>Price</DataTable.Title>
             <DataTable.Title numeric>Action</DataTable.Title>
           </DataTable.Header>
-          {
-            distinctItems.map((item, index, self) =>
+          <ScrollView>
+            {
+              distinctItems.map((item, index, self) =>
 
-              <DataTable.Row key={index}>
-                <DataTable.Cell>{index + 1}</DataTable.Cell>
-                <DataTable.Cell style={{ marginRight: 8 }}>{item.name}</DataTable.Cell>
-                <DataTable.Cell numeric>
-                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <DataTable.Row key={index} style={{ marginVertical: 16 }}>
+                  <DataTable.Cell>{index + 1}</DataTable.Cell>
+                  <DataTable.Cell style={{ marginRight: 8 }}>{item.name}</DataTable.Cell>
+                  <DataTable.Cell numeric>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <TouchableRipple
+                        onPress={() => handlePlusBtn(item.id)}
+                        rippleColor="rgba(0, 0, 0, .32)"
+                        style={{ padding: 4 }}
+                      >
+                        <MaterialCommunityIcons name="plus" color="#000" size={12} />
+                      </TouchableRipple>
+
+                      <Text style={{ paddingHorizontal: 4 }}>{counts[item.name]}</Text>
+                      <TouchableRipple
+                        onPress={() => handleMinusBtn(item.id)}
+                        rippleColor="rgba(0, 0, 0, .32)"
+                        style={{ padding: 4 }}
+                      >
+                        <MaterialCommunityIcons name="minus" color="#000" size={12} />
+                      </TouchableRipple>
+                    </View>
+                  </DataTable.Cell>
+                  <DataTable.Cell numeric>{counts[item.name] * item.price}</DataTable.Cell>
+                  <DataTable.Cell numeric>
                     <TouchableRipple
-                      onPress={() => console.log('Pressed')}
+                      onPress={() => handleRemoveItems(item.name)}
                       rippleColor="rgba(0, 0, 0, .32)"
                       style={{ padding: 4 }}
                     >
-                      <MaterialCommunityIcons name="plus" color="#000" size={12} />
+                      <MaterialCommunityIcons name="close-circle" color="red" size={14} />
                     </TouchableRipple>
 
-                    <Text style={{ paddingHorizontal: 4 }}>{counts[item.name]}</Text>
-                    <TouchableRipple
-                      onPress={() => console.log('Pressed')}
-                      rippleColor="rgba(0, 0, 0, .32)"
-                      style={{ padding: 4 }}
-                    >
-                      <MaterialCommunityIcons name="minus" color="#000" size={12} />
-                    </TouchableRipple>
-                  </View>
-                </DataTable.Cell>
-                <DataTable.Cell numeric>{counts[item.name] * item.price}</DataTable.Cell>
-                <DataTable.Cell numeric>
-                  <TouchableRipple
-                    onPress={() => console.log('Pressed')}
-                    rippleColor="rgba(0, 0, 0, .32)"
-                    style={{ padding: 4 }}
-                  >
-                    <MaterialCommunityIcons name="close-circle" color="red" size={14} />
-                  </TouchableRipple>
+                  </DataTable.Cell>
+                </DataTable.Row>
 
-                </DataTable.Cell>
-              </DataTable.Row>
-
-            )
-          }
+              )
+            }
+          </ScrollView>
         </DataTable>
 
 
